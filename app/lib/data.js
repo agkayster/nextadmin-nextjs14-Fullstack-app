cf45b4826a3b47cc5b309d3d2ead91b9f0805d9b
agkayster <ejike.chiboka@gmail.com>
2024-01-10 15:47:28 +0100

    Implementation of Pagination for Users component

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,13 +1,22 @@
 import { User } from './models';
 import { connectToDB } from './utils';
 
-export const fetchUsers = async (q) => {
+export const fetchUsers = async (q, page) => {
 	const regex = new RegExp(q, 'i');
 
+	/* list or number of people per page */
+	const ITEM_PER_PAGE = 2;
+
+	/* skip enables us to move to the next page in our pagination. you can use .count() or .countDocuments() below */
 	try {
 		connectToDB();
-		const users = await User.find({ username: { $regex: regex } });
-		return users;
+		const count = await User.find({
+			username: { $regex: regex },
+		}).count();
+		const users = await User.find({ username: { $regex: regex } })
+			.limit(ITEM_PER_PAGE)
+			.skip(ITEM_PER_PAGE * (page - 1));
+		return { count, users };
 	} catch (error) {
 		console.log(error);
 		throw new Error(error);
--- app/ui/dashboard/pagination/pagination.jsx
+++ app/ui/dashboard/pagination/pagination.jsx
@@ -1,16 +1,51 @@
+'use client';
+
 import React from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 import styles from './pagination.module.css';
 
-const Pagination = () => {
+const Pagination = ({ count }) => {
+	const searchParams = useSearchParams();
+	/* replace is used to replace our pathname and add search query */
+	const { replace } = useRouter();
+	const pathname = usePathname();
+	const params = new URLSearchParams(searchParams);
+
+	const page = searchParams.get('page') || 1;
+
+	/* here we set how many items we want to see per page on our app */
+	const ITEM_PER_PAGE = 2;
+
+	/* calculates how we get the previous pages */
+	const hasPrev = ITEM_PER_PAGE * (parseInt(page) - 1) > 0;
+
+	/* calculates how we get the next page */
+	const hasNext =
+		ITEM_PER_PAGE * (parseInt(page) - 1) + ITEM_PER_PAGE < count;
+
+	const handleChangePage = (type) => {
+		/* if we click on a button that has "prev", it should take us back one page else take us forward */
+		type === 'prev'
+			? params.set('page', parseInt(page) - 1)
+			: params.set('page', parseInt(page) + 1);
+
+		/* replace edits our url/pathname by adding query params */
+		replace(`${pathname}?${params}`);
+	};
+
 	return (
 		<div className='cont p-2.5 flex justify-between'>
 			<button
 				className={`prevBtn ${styles.button} py-1.5 px-2.5 cursor-pointer`}
-				disabled>
+				disabled={!hasPrev}
+				onClick={() => handleChangePage('prev')}>
 				Previous
 			</button>
 			<button
-				className={`nextBtn ${styles.button} py-1.5 px-2.5 cursor-pointer bg-white text-black`}>
+				className={`nextBtn ${styles.button} py-1.5 px-2.5 cursor-pointer bg-white text-black`}
+				disabled={!hasNext}
+				onClick={() => handleChangePage('next')}>
 				Next
 			</button>
 		</div>
