f3c5dd3de12825de207e3515ce66682b67564248
agkayster <ejike.chiboka@gmail.com>
2024-01-11 10:05:41 +0100

    Implementation of Nextjs14 server actions

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,9 +1,12 @@
+import { addUser } from '@/app/lib/actions';
 import React from 'react';
 
 const AddUsersPage = () => {
 	return (
 		<div className='cont bg-[#182237] p-5 rounded-xl mt-5'>
-			<form className='form flex flex-wrap justify-between'>
+			<form
+				action={addUser}
+				className='form flex flex-wrap justify-between'>
 				<input
 					className='username p-8 w-[45%] bg-[#151c2c] text-white rounded-md mb-8 border-solid 
 					border-2 border-[#2e374a]'
@@ -40,9 +43,7 @@ const AddUsersPage = () => {
 					id='isAdmin'
 					className='p-8 bg-[#151c2c] text-white rounded-md mb-8 border-solid 
 					border-2 border-[#2e374a] w-[45%]'>
-					<option value={false} selected>
-						Is Admin?
-					</option>
+					<option value={false}>Is Admin?</option>
 					<option value={false}>No</option>
 					<option value={true}>Yes</option>
 				</select>
@@ -51,9 +52,7 @@ const AddUsersPage = () => {
 					id='isActive'
 					className='p-8 bg-[#151c2c] text-white rounded-md mb-8 border-solid 
 					border-2 border-[#2e374a] w-[45%]'>
-					<option value={true} selected>
-						Is Active?
-					</option>
+					<option value={true}>Is Active?</option>
 					<option value={false}>No</option>
 					<option value={true}>Yes</option>
 				</select>
