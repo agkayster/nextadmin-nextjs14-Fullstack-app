5303f3df9305d4d0b55a183f22403c45919235da
agkayster <ejike.chiboka@gmail.com>
2024-01-12 09:00:12 +0100

    Implemented Password hashing

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,5 +1,6 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import bcrypt from 'bcrypt';
 
 import { User } from './models';
 import { connectToDB } from './utils';
@@ -9,12 +10,21 @@ export const addUser = async (formData) => {
 	const { username, email, password, phone, address, isAdmin, isActive } =
 		Object.fromEntries(formData);
 
+	const saltRounds = 10;
+
 	try {
 		connectToDB();
+
+		const salt = await bcrypt.genSalt(saltRounds);
+
+		/* hash our password using salt */
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		/*pass our hashed password into our model, which is then saved in our Database */
 		const newUser = new User({
 			username,
 			email,
-			password,
+			password: hashedPassword,
 			phone,
 			address,
 			isAdmin,
