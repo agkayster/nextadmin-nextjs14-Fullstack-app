76d423a03760db748076d34a7b57a05af2d11063
agkayster <ejike.chiboka@gmail.com>
2024-01-17 22:04:03 +0100

    Dynamic implementation of users count on dashboard

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { MdSupervisedUserCircle } from 'react-icons/md';
+import { fetchUsers } from '@/app/lib/data';
 
-const CardComponent = () => {
+const CardComponent = async ({ searchParams }) => {
+	const q = searchParams?.q || '';
+	const page = searchParams?.page || 1;
+
+	const { count } = await fetchUsers(q, page);
 	return (
 		<div className='cont flex gap-5 bg-[#182237] p-5 rounded-xl cursor-pointer w-full hover:bg-[#2e374a]'>
 			<MdSupervisedUserCircle size={24} />
 			<div className='texts flex flex-col gap-5'>
 				<span className='title'>Total Users</span>
-				<span className='number text-2xl font-medium'>10.273</span>
+				<span className='number text-2xl font-medium'>{count}</span>
 				<span className='detail text-xs font-light'>
 					<span className='positive text-lime-500'>12%</span> more
 					than previous week
